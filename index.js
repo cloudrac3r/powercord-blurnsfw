a0b4c60c2528290758ec242d03f3c4279512d0a5
Cadence Fish <cloudrac3r@vivaldi.net>
2019-06-24 02:48:11 +1200

    1.0.0

---
+++ index.js
@@ -0,0 +1,58 @@
+const { Plugin } = require("powercord/entities")
+const webpack = require("powercord/webpack")
+const { getModuleByDisplayName } = webpack
+const { inject, uninject } = require("powercord/injector")
+const { getOwnerInstance } = require("powercord/util")
+const { resolve} = require("path")
+
+module.exports = class BlurNSFW extends Plugin {
+	constructor () {
+		super()
+	}
+
+	async startPlugin () {
+		this.loadCSS(resolve(__dirname, "style.scss"))
+		this.getChannel = (await webpack.getModule(["getChannel"])).getChannel
+		this.patchedRenderer = false
+		this._patchChannelList()
+	}
+
+	pluginWillUnload () {
+		uninject("cadence-blurnsfw-channels")
+		uninject("cadence-blurnsfw-channellist")
+	}
+
+	async _patchChannelList() {
+		let _this = this;
+		const Channels = await getModuleByDisplayName("Channels")
+		inject("cadence-blurnsfw-channellist", Channels.prototype, "render", function(_, res) {
+			let chat = document.querySelector(".pc-chat")
+			if (chat && !_this.patchedRenderer) {
+				_this._patchChannelRenderer(chat)
+				_this.patchedRenderer = true
+				uninject("cadence-blurnsfw-channellist")
+			}
+			return res
+		})
+	}
+
+	async _patchChannelRenderer(chat) {
+		let _this = this
+		const Channel = getOwnerInstance(chat)
+		inject("cadence-blurnsfw-channels", Channel.constructor.prototype, "render", function(_, res) {
+			let currentChannelID = res.props.children[1].props.channelId
+			let channel = _this.getChannel(currentChannelID)
+			if (channel.nsfw) {
+				res.props.className += " is-nsfw-channel"
+			}
+			document.querySelectorAll(".pc-imageWrapper").forEach(embedImage => {
+				if (embedImage.lastChild && embedImage.lastChild.tagName != "DIV") {
+					let nsfwLabel = document.createElement("div")
+					nsfwLabel.classList.add("nsfw-block")
+					embedImage.appendChild(nsfwLabel)
+				}
+			})
+			return res
+		})
+	}
+}
