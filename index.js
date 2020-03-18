aa7cd81d59dc394ac4a8e11879abe2d4ae8070da
Marvin Witt <nurmarvin@protonmail.com>
2020-03-18 14:49:09 +0100

    Version 1.1.0
    - better use of SCSS
    - fix for recent discord update that fucked this plugin
    - fix for videos: used to go blurred while playing when not hovered anymore

--- index.js
+++ index.js
@@ -1,51 +1,51 @@
-const { Plugin } = require("powercord/entities")
-const webpack = require("powercord/webpack")
-const { getModuleByDisplayName } = webpack
-const { inject, uninject } = require("powercord/injector")
-const { getOwnerInstance } = require("powercord/util")
-const { resolve} = require("path")
+const { Plugin } = require('powercord/entities');
+const webpack = require('powercord/webpack');
+const { getModuleByDisplayName } = webpack;
+const { inject, uninject } = require('powercord/injector');
+const { getOwnerInstance } = require('powercord/util');
+const { resolve } = require('path');
 
 module.exports = class BlurNSFW extends Plugin {
-	constructor () {
-		super()
-	}
+  async startPlugin() {
+    this.loadCSS(resolve(__dirname, 'style.scss'));
+    this.getChannel = (await webpack.getModule(['getChannel'])).getChannel;
+    this.patchedRenderer = false;
+    this._patchChannelList();
+  }
 
-	async startPlugin () {
-		this.loadCSS(resolve(__dirname, "style.scss"))
-		this.getChannel = (await webpack.getModule(["getChannel"])).getChannel
-		this.patchedRenderer = false
-		this._patchChannelList()
-	}
+  pluginWillUnload() {
+    uninject('cadence-blurnsfw-channels');
+    uninject('cadence-blurnsfw-channellist');
+  }
 
-	pluginWillUnload () {
-		uninject("cadence-blurnsfw-channels")
-		uninject("cadence-blurnsfw-channellist")
-	}
+  async _patchChannelList() {
+    const _this = this;
+    const chatClass = (await webpack.getModule(['chat'], false)).chat;
+    const ChannelItem = await getModuleByDisplayName('ChannelItem');
 
-	async _patchChannelList() {
-		let _this = this;
-		const Channels = await getModuleByDisplayName("Channels")
-		inject("cadence-blurnsfw-channellist", Channels.prototype, "render", function(_, res) {
-			let chat = document.querySelector(".chat-3bRxxu")
-			if (chat && !_this.patchedRenderer) {
-				_this._patchChannelRenderer(chat)
-				_this.patchedRenderer = true
-				uninject("cadence-blurnsfw-channellist")
-			}
-			return res
-		})
-	}
+    inject('cadence-blurnsfw-channellist', ChannelItem.prototype, 'render', (_, res) => {
+      const chat = document.querySelector(`.${chatClass}`);
+      if (chat && !_this.patchedRenderer) {
+        _this._patchChannelRenderer(chat);
+        _this.patchedRenderer = true;
+        uninject('cadence-blurnsfw-channellist');
+      }
+      return res;
+    });
+  }
 
-	async _patchChannelRenderer(chat) {
-		let _this = this
-		const Channel = getOwnerInstance(chat)
-		inject("cadence-blurnsfw-channels", Channel.constructor.prototype, "render", function(_, res) {
-			let currentChannelID = res.props.children[1].props.channelId
-			let channel = _this.getChannel(currentChannelID)
-			if (channel.nsfw) {
-				res.props.className += " is-nsfw-channel"
-			}
-			return res
-		})
-	}
-}
+  async _patchChannelRenderer(chat) {
+    const _this = this;
+    const Channel = getOwnerInstance(chat);
+    inject('cadence-blurnsfw-channels', Channel.constructor.prototype, 'render', (_, res) => {
+      const currentChannelID = res.props.children[1].props.channelId;
+      const channel = _this.getChannel(currentChannelID);
+
+      if (channel.nsfw) {
+        res.props.className += ' nsfw';
+      }
+
+      return res;
+    });
+  }
+};
